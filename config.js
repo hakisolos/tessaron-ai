0080a6dd1e46e8e948d2ff305f1b4dae33cd90f4
hakisolos <165590443+hakisolos@users.noreply.github.com>
2025-03-21 18:13:34 +0100

    GOD DID

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
     prefix: process.env.prefix || "?",
     pastekey: process.env.pastekey || "VvgHwglwdovnKbUgjG0H-zW7lYMgmCO6",
     SESSION_ID: process.env.SESSION_ID || "",
-    MONGO_URL: process.env.MONGO_URL || "mongodb+srv://maxwellexcel174:<db_password>@hakixer.sqaab.mongodb.net/?retryWrites=true&w=majority&appName=hakixer",
+    MONGO_URL: process.env.MONGO_URL || "mongodb+srv://whatsbixby:whatsbixby@cluster0.idp3t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     public: toBool(process.env.public) ||  "false",
     sudo: process.env.sudo || ["2349112171078@s.whatsapp.net"]
 }
